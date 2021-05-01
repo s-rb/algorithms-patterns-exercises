669fa14adce7e62ef192d1f652962bd7f3d59db7
Roman Surkov <s.rb@list.ru>
2021-05-01 12:10:27 +0900

    Refactoring

---
+++ src/ru/list/surkovr/data_structures/double_linked_list/ListItem.java
@@ -0,0 +1,35 @@
+package ru.list.surkovr.data_structures.double_linked_list;
+
+public class ListItem
+{
+    private String data;
+    private ListItem prev;
+    private ListItem next;
+
+    public ListItem(String data) { this.data = data; }
+
+    public String getData()
+    {
+        return data;
+    }
+
+    public void setNext(ListItem item)
+    {
+        next = item;
+    }
+
+    public ListItem getNext()
+    {
+        return next;
+    }
+
+    public void setPrev(ListItem item)
+    {
+        prev = item;
+    }
+
+    public ListItem getPrev()
+    {
+        return prev;
+    }
+}
---
+++ src/ru/list/surkovr/sortAlgorithms/merge_sort/MergeSort.java
@@ -0,0 +1,119 @@
+package ru.list.surkovr.sortAlgorithms.merge_sort;
+
+public class MergeSort {
+    public static void mergeSort(int[] array) {
+        int n = array.length;
+        int middle = n / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[n - middle];
+
+        for (int i = 0; i < middle; i++) {
+            leftArray[i] = array[i];
+        }
+        for (int i = middle; i < n; i++) {
+            rightArray[i - middle] = array[i];
+        }
+        if (leftArray.length > 1) {
+            mergeSort(leftArray);
+        }
+        if (rightArray.length > 1) {
+            mergeSort(rightArray);
+        }
+        merge(array, leftArray, rightArray);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        if (array.length == 1) {
+            if (left.length == 1 && right.length == 0) {
+                array[0] = left[0];
+            } else if (right.length == 1 && left.length == 0) {
+                array[0] = right[0];
+            }
+            return;
+        }
+        int leftIndex = 0;
+        int rightIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (leftIndex < left.length) {
+                if (rightIndex == right.length) {
+                    array[i] = left[leftIndex];
+                    leftIndex += 1;
+                } else if (left[leftIndex] < right[rightIndex]) {
+                    array[i] = left[leftIndex];
+                    leftIndex += 1;
+                } else {
+                    array[i] = right[rightIndex];
+                    rightIndex += 1;
+                }
+            } else {
+                array[i] = right[rightIndex];
+                rightIndex += 1;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+// public static void mergeSort(int[] array) {
+//        System.out.println("Массив на входе: ");
+//        System.out.println(Arrays.toString(array));
+//        int n = array.length;
+//        int middle = n / 2;
+//        int[] leftArray = new int[middle];
+//        int[] rightArray = new int[n - middle];
+//
+//        for (int i = 0; i < middle; i++) {
+//            leftArray[i] = array[i];
+//        }
+//        for (int i = middle; i < n; i++) {
+//            rightArray[i - middle] = array[i];
+//        }
+//        if (leftArray.length > 1) {
+//            mergeSort(leftArray);
+//        }
+//        if (rightArray.length > 1) {
+//            mergeSort(rightArray);
+//        }
+//        System.out.println("Запускаем merge ");
+//        merge(array, leftArray, rightArray);
+//    }
+//
+//    private static void merge(int[] array, int[] left, int[] right) {
+//        System.out.println("Получили массивы: ");
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(Arrays.toString(left));
+//        System.out.println(Arrays.toString(right));
+//        if (array.length == 1) {
+//            if (left.length == 1 && right.length == 0) {
+//                array[0] = left[0];
+//            } else if (right.length == 1 && left.length == 0) {
+//                array[0] = right[0];
+//            }
+//            return;
+//        }
+//        int leftIndex = 0;
+//        int rightIndex = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (leftIndex < left.length) { // Еще есть элементы в левом массиве к которым не обращались
+//                if (rightIndex == right.length) { // Последний элемент прошли
+//                    array[i] = left[leftIndex];
+//                    leftIndex += 1;
+//                } else if (left[leftIndex] < right[rightIndex]) {
+//                    array[i] = left[leftIndex];
+//                    leftIndex += 1;
+//                } else {
+//                    array[i] = right[rightIndex];
+//                    rightIndex += 1;
+//                }
+//            } else {
+//                array[i] = right[rightIndex];
+//                rightIndex += 1;
+//            }
+//        }
+//        System.out.println("Отсортировали массив до ");
+//        System.out.println(Arrays.toString(array));
+//    }
