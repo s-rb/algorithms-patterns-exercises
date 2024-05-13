944b2d4dd6e6caa32889d0bf4876399b87cce30f
Roman Surkov <s.rb@list.ru>
2024-05-13 18:02:12 +0200

    Depth first search - Task 3 - average of subtree

--- src/main/java/ru/list/surkovr/algorithms/dfs/DepthFirstSearch.java
+++ src/main/java/ru/list/surkovr/algorithms/dfs/DepthFirstSearch.java
@@ -120,4 +120,88 @@ public class DepthFirstSearch {
         visited[i] = 1;
         return true;
     }
+
+    /**
+     * 2265. Count Nodes Equal to Average of Subtree
+     * Medium
+     * Topics
+     * Companies
+     * Hint
+     * Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+     *
+     * Note:
+     *
+     * The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+     * A subtree of root is a tree consisting of root and all of its descendants.
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: root = [4,8,5,0,1,null,6]
+     * Output: 5
+     * Explanation:
+     * For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+     * For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+     * For the node with value 0: The average of its subtree is 0 / 1 = 0.
+     * For the node with value 1: The average of its subtree is 1 / 1 = 1.
+     * For the node with value 6: The average of its subtree is 6 / 1 = 6.
+     * Example 2:
+     *
+     *
+     * Input: root = [1]
+     * Output: 1
+     * Explanation: For the node with value 1: The average of its subtree is 1 / 1 = 1.
+     *
+     *
+     * Constraints:
+     *
+     * The number of nodes in the tree is in the range [1, 1000].
+     * 0 <= Node.val <= 1000
+     */
+    private static int countedAvgEqualNodes = 0;
+
+    public static int averageOfSubtree(TreeNode root) {
+        dfs(root);
+        return countedAvgEqualNodes;
+    }
+
+    private static int[] dfs(TreeNode node) {
+        int[] data = new int[]{1, node.val};
+        if (node.left != null) {
+            int[] temp = dfs(node.left);
+            data[0] = temp[0] + data[0];
+            data[1] = temp[1] + data[1];
+        }
+        if (node.right != null) {
+            int[] temp = dfs(node.right);
+            data[0] = temp[0] + data[0];
+            data[1] = temp[1] + data[1];
+        }
+        if (node.val == data[1] / data[0]) countedAvgEqualNodes++;
+        return data;
+    }
+
+    public static int averageOfSubtree2(TreeNode root) {
+        int[] res = dfs2(root);
+        return res[2];
+    }
+
+    private static int[] dfs2(TreeNode node) {
+        int[] data = new int[]{1, node.val, 0};
+        if (node.left != null) {
+            int[] temp = dfs2(node.left);
+            data[0] = temp[0] + data[0];
+            data[1] = temp[1] + data[1];
+            data[2] = temp[2] + data[2];
+        }
+        if (node.right != null) {
+            int[] temp = dfs2(node.right);
+            data[0] = temp[0] + data[0];
+            data[1] = temp[1] + data[1];
+            data[2] = temp[2] + data[2];
+        }
+        if (node.val == data[1] / data[0]) data[2] = data[2] + 1;
+        return data;
+    }
 }
--- src/test/java/ru/list/surkovr/algorithms/dfs/DepthFirstSearchTest.java
+++ src/test/java/ru/list/surkovr/algorithms/dfs/DepthFirstSearchTest.java
@@ -2,6 +2,7 @@ package ru.list.surkovr.algorithms.dfs;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.list.surkovr.algorithms.dfs.DepthFirstSearch.TreeNode;
 
 import java.util.List;
 
@@ -11,8 +12,8 @@ class DepthFirstSearchTest {
 
     @Test
     void inorderTraversal() {
-        DepthFirstSearch.TreeNode root = new DepthFirstSearch.TreeNode(1, null,
-                new DepthFirstSearch.TreeNode(2, new DepthFirstSearch.TreeNode(3), null));
+        TreeNode root = new TreeNode(1, null,
+                new TreeNode(2, new TreeNode(3), null));
 
         Assertions.assertThat(DepthFirstSearch.inorderTraversal(root)).isEqualTo(List.of(1,3,2));
     }
@@ -22,5 +23,22 @@ class DepthFirstSearchTest {
         Assertions.assertThat(DepthFirstSearch.eventualSafeNodes(
                 new int[][] {{1,2},{2,3},{5},{0},{5},{},{}}))
                 .isEqualTo(List.of(2,4,5,6));
+    }
+
+    @Test
+    void averageOfSubtree() {
+        TreeNode root = new TreeNode(4,
+                new TreeNode(8, new TreeNode(0), new TreeNode(1)),
+                new TreeNode(5, null, new TreeNode(6)));
+        Assertions.assertThat(DepthFirstSearch.averageOfSubtree(root))
+                .isEqualTo(5);
+    }
+    @Test
+    void averageOfSubtree2() {
+        TreeNode root = new TreeNode(4,
+                new TreeNode(8, new TreeNode(0), new TreeNode(1)),
+                new TreeNode(5, null, new TreeNode(6)));
+        Assertions.assertThat(DepthFirstSearch.averageOfSubtree2(root))
+                .isEqualTo(5);
     }
 }
