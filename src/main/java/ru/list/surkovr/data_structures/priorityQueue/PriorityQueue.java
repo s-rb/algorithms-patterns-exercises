4401a506f5d1965f3509d2ffaad0926296cb4e56
Roman Surkov <s.rb@list.ru>
2024-06-03 19:04:37 +0200

    PriorityQueue - Task 2 - frequencySort

--- src/main/java/ru/list/surkovr/data_structures/priorityQueue/PriorityQueue.java
+++ src/main/java/ru/list/surkovr/data_structures/priorityQueue/PriorityQueue.java
@@ -2,6 +2,8 @@ package ru.list.surkovr.data_structures.priorityQueue;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PriorityQueue {
 
@@ -54,5 +56,54 @@ public class PriorityQueue {
         for (int num : nums) queue.add(num);
         for (int i = 0; i < nums.length; i++) nums[i % 2 != 0 ? (i - 1) : (i + 1)] = queue.poll();
         return nums;
+    }
+
+    /**
+     * 451. Sort Characters By Frequency
+     * Medium
+     * Topics
+     * Companies
+     * Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+     *
+     * Return the sorted string. If there are multiple answers, return any of them.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: s = "tree"
+     * Output: "eert"
+     * Explanation: 'e' appears twice while 'r' and 't' both appear once.
+     * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+     * Example 2:
+     *
+     * Input: s = "cccaaa"
+     * Output: "aaaccc"
+     * Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+     * Note that "cacaca" is incorrect, as the same characters must be together.
+     * Example 3:
+     *
+     * Input: s = "Aabb"
+     * Output: "bbAa"
+     * Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+     * Note that 'A' and 'a' are treated as two different characters.
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= s.length <= 5 * 105
+     * s consists of uppercase and lowercase English letters and digits.
+     */
+    public static String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (Character c : s.toCharArray()) map.put(c, map.getOrDefault(c, 0)+1);
+        java.util.PriorityQueue<Map.Entry<Character, Integer>> queue = new java.util.PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
+        queue.addAll(map.entrySet());
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            var e = queue.poll();
+            sb.append(String.valueOf(e.getKey()).repeat(e.getValue()));
+        }
+        return sb.toString();
     }
 }
