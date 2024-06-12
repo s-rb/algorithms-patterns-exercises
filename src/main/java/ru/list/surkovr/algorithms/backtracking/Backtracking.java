1daa47dd1e196b26f94e19813fe34d9041c3985b
Roman Surkov <s.rb@list.ru>
2024-06-12 22:00:52 +0200

    Backtracking - Task 3 - all paths from source to target

--- src/main/java/ru/list/surkovr/algorithms/backtracking/Backtracking.java
+++ src/main/java/ru/list/surkovr/algorithms/backtracking/Backtracking.java
@@ -161,4 +161,58 @@ public class Backtracking {
             list.remove(list.size()-1);
         }
     }
+
+    /**
+     * 797. All Paths From Source to Target
+     * Medium
+     * Topics
+     * Companies
+     * Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+     *
+     * The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: graph = [[1,2],[3],[3],[]]
+     * Output: [[0,1,3],[0,2,3]]
+     * Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+     * Example 2:
+     *
+     *
+     * Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+     * Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+     *
+     *
+     * Constraints:
+     *
+     * n == graph.length
+     * 2 <= n <= 15
+     * 0 <= graph[i][j] < n
+     * graph[i][j] != i (i.e., there will be no self-loops).
+     * All the elements of graph[i] are unique.
+     * The input graph is guaranteed to be a DAG.
+     */
+    private static List<List<Integer>> res = new ArrayList<>();
+    public static List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        doBackTrack(graph, new ArrayList<>(){{add(0);}}, 0);
+        return res;
+    }
+
+    private static void doBackTrack(int[][] graph, List<Integer> path, int lastIndex) {
+        if (lastIndex == graph.length-1) {
+            res.add(new ArrayList<>(path));
+            path.clear();
+            return;
+        }
+
+        int[] subset = graph[lastIndex];
+        for (int i = 0; i < subset.length; i++) {
+            path.add(subset[i]);
+            doBackTrack(graph, path, subset[i]);
+            path.remove(path.size()-1);
+        }
+    }
 }
