c27d3187f922783fef656f54231247fc833d70bd
Roman Surkov <s.rb@list.ru>
2024-07-07 11:08:38 +0200

    Greedy - Task 2 - Partitioning Into Minimum Number Of Deci-Binary Numbers

--- src/main/java/ru/list/surkovr/algorithms/greedy/Greedy.java
+++ src/main/java/ru/list/surkovr/algorithms/greedy/Greedy.java
@@ -92,4 +92,53 @@ public class Greedy {
         for (int el : elements) max = Math.max(max, el);
         return max;
     }
+
+    /**
+     * 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+     * Medium
+     * Topics
+     * Companies
+     * Hint
+     * A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+     *
+     * Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: n = "32"
+     * Output: 3
+     * Explanation: 10 + 11 + 11 = 32
+     * Example 2:
+     *
+     * Input: n = "82734"
+     * Output: 8
+     * Example 3:
+     *
+     * Input: n = "27346209830709182346"
+     * Output: 9
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= n.length <= 105
+     * n consists of only digits.
+     * n does not contain any leading zeros and represents a positive integer.
+     */
+    public static int minPartitions(String n) {
+        char max = '0';
+        for (char c : n.toCharArray()) {
+            max = c > max ? c : max;
+            if (max == '9') break;
+        }
+        return Character.getNumericValue(max);
+    }
+
+    public static int minPartitions2(String n) {
+        for (int i = 9; i >=0; i--) {
+            if (n.contains(String.valueOf(i))) return i;
+        }
+        return 0;
+    }
 }
